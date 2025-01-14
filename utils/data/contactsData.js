1e4a7d74a567e72131c05b98999cca11ef38cbf6
penscola <penscolashackletonfelix@gmail.com>
2025-01-14 23:38:34 +0300

    Personal data

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'penscolashackletonfelix@gmail.com',
+    phone: '+254111537414',
+    address: 'Nairobi, Kenya',
+    github: 'https://github.com/penscola',
+    facebook: 'https://web.facebook.com/penscola.shackleton/',
+    linkedIn: 'https://www.linkedin.com/in/felix-kiprotich-a2ba1a1a4/',
+    twitter: 'https://x.com/PenscolaF',
+    stackOverflow: 'https://stackoverflow.com/users/19373285/penscola-shackleton-felix',
+    devUsername: "penscola"
 }
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Junior Machine Learning Engineer',
+    company: "Omdena-Antananarivo Local Chapter.",
+    duration: "(Nov 2023 - Feb 2024)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Data Scientist Intern",
+    company: "Bharat Intern",
+    duration: "(Oct 2023 - Nov 2023)"
   },
   {
     id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Back End Intern Developer",
+    company: "Podii Company Limited",
+    duration: "May 2023 - Aug 2023"
   }
 ]
