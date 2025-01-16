fae0078b5c2993c81e3e899b00eb9e4cb79d92b3
penscola <penscolashackletonfelix@gmail.com>
2025-01-16 18:44:00 +0300

    Reroute and editing

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,12 +12,12 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Abu Said</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/felix-kiprotich-a2ba1a1a4/" className="text-[#16f2b3]">Penscola@Tech</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
+              href="https://github.com/penscola/My_Portfolio"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
@@ -25,7 +25,7 @@ function Footer() {
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio/fork"
+              href="https://github.com/penscola/My_Portfolio/fork"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
